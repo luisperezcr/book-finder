bbdf1e5a32cfe45bac56443a4c512a87f5e61c42
Luis Pérez <luis.perez@geekdacity.com>
2020-03-28 16:00:07 -0600

    Generate book item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FinderService } from './finder/finder.service';
 import { SearchInputComponent } from './finder/components/search-input/search-input.component';
+import { BookItemComponent } from './finder/components/book-item/book-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchInputComponent
+    SearchInputComponent,
+    BookItemComponent
   ],
   imports: [
     BrowserModule,
