868ebd74d1c7f8b05cfceb0ad816e2dbd0fec195
Luis Pérez <luis.perez@geekdacity.com>
2020-03-29 11:00:23 -0600

    Remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { FinderService } from './finder/finder.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'book-finder';
-
-  constructor(private finderService: FinderService) {}
-
-  getBooks() {
-    this.finderService.getBooks();
-  }
-}
+export class AppComponent {}
